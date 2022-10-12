4e41ec34de9eeb5295cd1c8bda79246275d37437
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2022-10-12 10:46:43 -0300

    renomeando arquivos

---
+++ 3_Core_Modules/9_renomear_arquivos/index.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const oldName = "arquivo.txt"
+
+const newName =  "novoarquivo.txt"
+
+fs.rename(oldName, newName, function(err){
+
+    if(err){
+        console.log(err)
+        return
+    }
+
+    console.log(`O arquivo ${oldName} foi renomeado para ${newName}`)
+
+})
