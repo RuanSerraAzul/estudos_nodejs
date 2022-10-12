a6ca1c437bd76a82f6a3be6c806a8dec6c9696b3
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2022-10-12 15:11:58 -0300

    modulo os

---
+++ 3_Core_Modules/15_modulo_os/index.js
@@ -0,0 +1,9 @@
+const os = require("os");
+
+console.log(os.cpus());
+console.log(os.hostname());
+console.log(os.userInfo());
+console.log(os.platform());
+console.log(os.freemem());
+console.log(os.homedir());
+console.log(os.type());
