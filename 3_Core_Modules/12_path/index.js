f9d7b8e5dffb8b33ce17190309f8f0285bba3ab8
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2022-10-12 14:33:42 -0300

    módulo path

---
+++ 3_Core_Modules/12_path/index.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+const customPath = "/relatorios/ruan/relatorio1.pdf";
+
+console.log(path.dirname(customPath));
+
+console.log(path.basename(customPath));
+
+console.log(path.extname(customPath));
