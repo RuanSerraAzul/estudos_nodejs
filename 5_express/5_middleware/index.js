db50ee1ab82f8ae542dc7b9cafec6cc5049439d6
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2022-10-14 18:34:35 -0300

    conhecendo o middleware

---
+++ 5_express/5_middleware/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+const basePath = path.join(__dirname, "templates");
+
+const checkAuth = function (req, res, next) {
+    req.authStatus = false;
+
+    if (req.authStatus) {
+        console.log(`Está logado, pode continuar`);
+        next();
+    } else {
+        console.log(`Não está logado, faça o login para continuar`);
+        next();
+    }
+};
+
+app.use(checkAuth);
+
+app.get("/", (req, res) => {
+    res.sendFile(`${basePath}/index.html`);
+});
+
+app.listen(port, () => {
+    console.log(`Aplicação rodando na porta ${port}`);
+});
