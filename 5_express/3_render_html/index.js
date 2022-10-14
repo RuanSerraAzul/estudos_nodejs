4e4d253ea27dc586e4a12fe6a5e0c6808ff95824
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2022-10-14 14:49:03 -0300

    render html

---
+++ 5_express/3_render_html/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+const basePath = path.join(__dirname, "templates");
+
+app.get("/", (req, res) => {
+    res.sendFile(`${basePath}/index.html`);
+});
+
+app.listen(port, () => {
+    console.log(`Aplicação rodando na porta ${port}`);
+});
