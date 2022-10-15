f2a374554ed2d04cbee3e25102f679f3e1919e36
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2022-10-15 19:41:58 -0300

    Página 404

---
+++ 5_express/10_404/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const basePath = path.join(__dirname, "templates");
+
+const users = require("./users");
+
+app.use("/users", users);
+
+app.get("/", (req, res) => {
+    res.sendFile(`${basePath}/index.html`);
+});
+
+app.use(function (req, res, next) {
+    res.status(404).sendFile(`${basePath}/404.html`);
+});
+
+app.listen(port, () => {
+    console.log(`Aplicação rodando na porta ${port}`);
+});
