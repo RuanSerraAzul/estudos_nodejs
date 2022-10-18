80485626d03af0636011730e7bb02dcac90a2d32
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2022-10-18 11:54:58 -0300

    instalacao sequelize

---
+++ 9_sequelize/1_instalacao/db/conn.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("nodesequelize", "root", "Ruan@123", {
+    host: "localhost",
+    dialect: "mysql",
+});
+
+try {
+    sequelize.authenticate();
+    console.log("Conectado ao Sequelize");
+} catch (err) {
+    console.log("Não foi possível conectar ao banco de dados, erro:", error);
+}
+
+module.exports = sequelize;
