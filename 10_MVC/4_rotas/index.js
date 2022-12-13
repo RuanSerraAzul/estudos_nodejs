6f49709cea276fcd70a89c80528fe0f2bb28dc30
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2022-12-12 23:35:08 -0300

    rotas

---
+++ 10_MVC/4_rotas/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+const conn = require("./db/conn");
+
+const Task = require("./models/Task");
+
+const taskRoutes = require("./routes/tasksRoutes");
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+app.use(express.json());
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.use("/tasks", taskRoutes);
+
+conn.sync()
+    .then(() => {
+        app.listen(3000);
+    })
+    .catch((err) => console.log(err));
