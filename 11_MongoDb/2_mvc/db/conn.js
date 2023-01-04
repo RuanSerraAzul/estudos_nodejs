c24140ffc4199e2b6c35a951bdb7bb362e5d8e22
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2023-01-04 11:54:39 -0300

    estrutura mvc

---
+++ 11_MongoDb/2_mvc/db/conn.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb://localhost:27017/testemongodb";
+
+const client = new MongoClient(uri);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Conectado ao MongoDb");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+run();
+
+module.exports = client;
