60e520bc95beec5cc7c24137f7e1461a17f46b2a
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2023-01-05 11:26:14 -0300

    Resgatando todos os dados

---
+++ 11_MongoDb/4_resgatando_dados/controllers/ProductController.js
@@ -0,0 +1,26 @@
+const { getProducts } = require("../models/Product");
+const Product = require("../models/Product");
+
+module.exports = class ProductController {
+    static async showProducts(req, res) {
+        const products = await getProducts();
+        res.render("products/all", { products });
+    }
+
+    static createProduct(req, res) {
+        res.render("products/create");
+    }
+
+    static createProductPost(req, res) {
+        const name = req.body.name;
+        const image = req.body.image;
+        const price = req.body.price;
+        const description = req.body.description;
+
+        const product = new Product(name, image, price, description);
+
+        product.save();
+
+        res.redirect("/products");
+    }
+};
